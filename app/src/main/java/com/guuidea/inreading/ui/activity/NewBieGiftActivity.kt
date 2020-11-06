c7d23f1b32aec746cf58856f4aa4bd766e77426c
jie.jiang <jie.jiang@jkohjkng.top>
2020-11-06 17:03:59 +0800

    规范化代码

--- app/src/main/java/com/guuidea/inreading/ui/activity/NewBieGiftActivity.kt
+++ app/src/main/java/com/guuidea/inreading/ui/activity/NewBieGiftActivity.kt
@@ -1,16 +1,17 @@
 package com.guuidea.inreading.ui.activity
 
+import com.guuidea.inreading.R
 import com.guuidea.inreading.ui.base.BaseActivity
 
 /**
  * @file      NewBieGiftActivity
- * @description    TODO
+ * @description    新人福利页面
  * @author         江 杰
  * @createDate     2020/11/6 10:10
  */
-class NewBieGiftActivity:BaseActivity(){
+class NewBieGiftActivity : BaseActivity() {
 
     override fun getContentId(): Int {
-        TODO("Not yet implemented")
+        return R.layout.activity_newbie_gift
     }
 }
